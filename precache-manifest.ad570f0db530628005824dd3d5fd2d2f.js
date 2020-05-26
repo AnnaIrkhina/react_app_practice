fc921580103ce3e01675a317a25e4603667e9cec
ponka123 <anka.irhina@gmail.com>
2020-05-26 14:55:14 -0700

    Updates

---
+++ precache-manifest.ad570f0db530628005824dd3d5fd2d2f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a361de2ca0031f2068ac1ca6272fa4c",
+    "url": "/react_app_practice/index.html"
+  },
+  {
+    "revision": "f490b0cbed55b3decd1e",
+    "url": "/react_app_practice/static/css/main.d1f3923a.chunk.css"
+  },
+  {
+    "revision": "72b833d3467de9d01e3d",
+    "url": "/react_app_practice/static/js/2.988400ae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_app_practice/static/js/2.988400ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f490b0cbed55b3decd1e",
+    "url": "/react_app_practice/static/js/main.0d5c63d2.chunk.js"
+  },
+  {
+    "revision": "bca4fbc3a2e9a33d1905",
+    "url": "/react_app_practice/static/js/runtime-main.641be997.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react_app_practice/static/media/logo.5d5d9eef.svg"
+  }
+]);
